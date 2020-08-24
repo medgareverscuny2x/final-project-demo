ef11eedce4c03b939b2ec5fb15fbd8fdd9d55d5a
Shahid <shahid.sarker@gmail.com>
2020-08-24 10:35:25 -0400

    add axios script, set up pokemon.js with pseuddocode to grab API data

---
+++ pokemon.js
@@ -0,0 +1,17 @@
+/**
+ grab from pokemon api:
+ - name
+ - id
+ - type
+ - heigh
+ - sprites (images)
+
+ *  */
+
+//  grab pokemon (by id?) from api
+// go through pokemon response
+// grab relevant data from response
+// assign the data to variables
+
+// target specific elements on the page (might have to refactor our html page)
+// update the elements with the API data
